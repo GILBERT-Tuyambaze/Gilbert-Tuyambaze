3cc72e2e68bef537b5c162e194e94c212e44a1f0
tuyambaze gilbert <145124748+GILBERT-Tuyambaze@users.noreply.github.com>
2025-08-10 21:32:06 +0200

    Update typed.js

--- js/typed.js
+++ js/typed.js
@@ -4,12 +4,39 @@
     loop: true
   });
 
-const menuBar = document.getElementById("menu-icon");
+/* ========= toggle icon navbar =========*/
+let menuIcon = document.querySelector('#mune-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
 
-menuBar.addEventListener("click", () => {console.log("hello");})
+/* ========= Scroll Sections Active Link =========*/
 
+let sections = document.querySelectorAll('section);
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if( top>= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+      });
+    };
+  });
+}
+                                  
+                                         
     
+
 
 
 
