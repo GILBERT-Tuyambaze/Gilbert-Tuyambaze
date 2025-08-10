8977b35e0b8263622b70f1c961a395679e1d3966
tuyambaze gilbert <145124748+GILBERT-Tuyambaze@users.noreply.github.com>
2025-08-10 23:06:50 +0200

    Update typed.js

--- js/typed.js
+++ js/typed.js
@@ -54,7 +54,11 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-content, .heading',{origin: 'top'});
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{origin: 'buttom'});
+ScrollReveal().reveal('.home-content h1, .about-img',{origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content',{origin: 'right'});
+
     
+
 
 
 
